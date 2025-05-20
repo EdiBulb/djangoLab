4022a514683925c22011a0e68fe15481988ed6af
Harry <89003206+EdiBulb@users.noreply.github.com>
2025-05-20 21:29:56 +1200

    update function implemented

--- myapp/views.py
+++ myapp/views.py
@@ -21,8 +21,11 @@ def HTMLTemplate(articleTag, id=None):
                     <input type="hidden" name="id" value={id}>
                     <input type = "submit" value="delete">
                 </form>
-            </li>       
-'''
+            </li>     
+            <li>
+                <a href="/update/{id}">update</a></li>
+            </li>  
+        '''
     ol = ''
     for topic in topics:
         ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
@@ -84,6 +87,34 @@ def create(request):
         return redirect(url)
     
 @csrf_exempt
+def update(request, id):
+    global topics
+    if request.method == 'GET':
+        for topic in topics:
+            if topic['id'] == int(id):
+                selectedTopic = {
+                    "title": topic['title'],
+                    "body": topic['body']
+                }
+        article = f'''
+        <form action="/update/{id}/" method="post">
+            <p><input type = "text" name = "title" placeholder="title" value={selectedTopic["title"]}></p>
+            <p><textarea name = "body" placeholder="body">{selectedTopic['body']}</textarea></p>
+            <p><input type = "submit"></p>
+        </form>
+        '''        
+        return HttpResponse(HTMLTemplate(article, id))
+    elif request.method == 'POST':
+        title = request.POST['title']
+        body = request.POST['body']
+        for topic in topics:
+            if topic['id'] == int(id):
+                topic['title'] = title
+                topic['body'] = body
+                
+        return redirect(f'/read/{id}')
+
+@csrf_exempt
 def delete(request):
     global topics
     if request.method == 'POST':
@@ -94,3 +125,4 @@ def delete(request):
                 newTopics.append(topic)
         topics = newTopics
         return redirect('/')
+
