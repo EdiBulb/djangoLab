17fdad42315be4ac19d111a6e1010bee39815586
Harry <89003206+EdiBulb@users.noreply.github.com>
2025-05-20 17:23:16 +1200

    Django Learning project

---
+++ myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render,HttpResponse
+import random
+# Create your views here.
+
+# send request to Customer.
+def index(request):
+    return HttpResponse('<h1>Random</h1>'+str(random.random()))
+
+def create(request):
+    return HttpResponse('Create')
+
+def read(request, id):
+    return HttpResponse('Read'+id)
